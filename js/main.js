c67817f70f37ef6aef8beddd49cbac916bcf54b4
Uqoug <gq280391457@gmail.com>
2016-12-08 23:37:51 +0800

    1208

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(".button").on("click", function(){
 	$(this).toggleClass("active");
 	$("#sidebar").add("#sidebox").toggleClass("slice");
-	console.log("1");
 })
+
+function clock() { 
+  var t = moment(),
+      a = t.minutes() * 6,
+      o = t.hours() % 12 / 12 * 360 + (a / 12);
+      s = t.second() * 6,
+  $(".hour").css("transform", "rotate(" + o + "deg)");
+  $(".minute").css("transform", "rotate(" + a + "deg)");
+  $(".second").css("transform", "rotate(" + s + "deg)");
+}
+function refreshClock() {
+  clock(), setTimeout(refreshClock, 100)
+}
+refreshClock();
