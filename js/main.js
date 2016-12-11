71903aeb967f835af417c743c3dc3c4c261d8206
Uqoug <gq280391457@gmail.com>
2016-12-11 22:08:52 +0800

    1211

--- js/main.js
+++ js/main.js
@@ -16,21 +16,26 @@ function clock(){
 }
 
 function refreshClock() {
-  clock(), setTimeout(refreshClock, 100)
+  clock(), setTimeout(refreshClock, 100);
 }
 refreshClock();
 
 $(".btn_artical").on("click",function(){
-  $.ajax({
-    url: "artical.html",
-    cache: false,
-    success: function(html){
-        $(".wraper").html(html);
-    }
-  });
-  $(".bar").toggleClass("barColorChange");
-  $("html").add("body").add(".wraper").css("overflow","visible");
-  $("#clock").css("opacity","0");
+
+    $.ajax({
+      url: "artical.html",
+      cache: false,
+      success: function(html){
+          $(".wraper").html(html);
+          $(".readmore").on("click",function(){
+            console.log('123');
+            $(this).prev(".contentWraper").toggleClass("showmore");
+          });
+      }
+    });
+    $(".bar").toggleClass("barColorChange");
+    $("html").add("body").add(".wraper").css("overflow","visible");
+    $("#clock").css("opacity","0");
 })
 
 $(".btn_homepage").on("click",function(){
@@ -53,12 +58,19 @@ $backToTop.hide();
 
 $(window).on('scroll', function() {
   if ($(this).scrollTop() > 100) {
-    $backToTop.fadeIn();
+    $backToTop.fadeIn("slow");
   } else {
-    $backToTop.fadeOut();
+    $backToTop.fadeOut("slow");
   }
 });
 
 $backToTop.on('click', function() {
   $("html, body").animate({scrollTop: 0}, 500);
+});
+
+
+// showmore
+$(".readmore").on("click",function(){
+  console.log('123');
+  $(this).prev(".contentWraper").toggleClass("showmore");
 });
